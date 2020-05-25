321b1232f877cd8c97cae41efdec2f0d7b0e83c1
Stephen Aitken <S00150371@mail.itsligo.ie>
2020-05-25 14:39:58 +0100

    .

---
+++ HDip_project_S00150371_V2/Pages/Privacy.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HDip_project_S00150371_V2.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
