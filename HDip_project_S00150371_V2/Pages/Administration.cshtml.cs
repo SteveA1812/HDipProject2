059fd833e621687742a569a6c9e7be3542c61008
Stephen Aitken <S00150371@mail.itsligo.ie>
2020-05-25 15:08:31 +0100

    Restart

---
+++ HDip_project_S00150371_V2/Pages/Administration.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HDip_project_S00150371_V2.Pages
+{
+    public class AdministrationModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
