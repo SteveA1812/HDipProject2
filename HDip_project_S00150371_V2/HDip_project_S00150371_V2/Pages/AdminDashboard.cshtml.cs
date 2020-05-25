d2b4488e2ac1d5111e0c4e370a6dede630fcfd89
Stephen Aitken <S00150371@mail.itsligo.ie>
2020-05-25 19:17:53 +0100

    Version 0.6

--- HDip_project_S00150371_V2/HDip_project_S00150371_V2/Pages/AdminDashboard.cshtml.cs
+++ HDip_project_S00150371_V2/HDip_project_S00150371_V2/Pages/AdminDashboard.cshtml.cs
@@ -2,17 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace HDip_project_S00150371_V2.Pages
 {
-    public class AboutModel : PageModel
+    public class AdminDashboardModel : PageModel
     {
-        public string Message { get; set; }
-
         public void OnGet()
         {
-            Message = "Your application description page.";
+
         }
     }
 }
